2b0a0b655a9faed164433206f981f79a58d3b447
Axel Ridray <axel.ridray@sfr.fr>
2021-05-03 18:32:27 +0200

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ User.destroy_all
 Offer.destroy_all
 Booking.destroy_all
 
-user1 = User.create(first_name: "Axel", last_name: "Ridray", email: "axel.ridray@sfr.fr", password:"123456")
-user2 = User.create(first_name: "Dam's", last_name: "menages", email: "menages.damien@gmail.com", password:" ")
+user1 = User.create!(first_name: "Axel", last_name: "Ridray", email: "axel.ridray@sfr.fr", password:"123456")
+user2 = User.create!(first_name: "Dam's", last_name: "menages", email: "menages.damien@gmail.com", password:"654321")
 
-offer1 = Offer.create(name:"super nitendo 64", description:"super console old school baby", price_per_day: 10.30, user: user2)
+offer1 = Offer.create!(name:"super nitendo 64", description:"super console old school baby", price_per_day: 10.30, user: user2)
 
-booking1 = Booking.create( user: user1,  offer: offer1, start_date: DateTime.now, end_date: DateTime.now + 2.days, total_price: 20.60, status: "accepted")
+booking1 = Booking.create!( user: user1,  offer: offer1, start_date: DateTime.now, end_date: DateTime.now + 2.days, total_price: 20.60, status: "accepted")
