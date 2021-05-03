73383abbb5268cb7826bad4fe03e0de89ae3f1cf
Damien Menages <menages.damien@gmail.com>
2021-05-03 18:09:12 +0200

    moved navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
   resources :offers do
   resources :bookings, only: [ :new, :create ]
   end
 
-  resources :booking, only: [ :show ] do 
-     # refacto 
+  resources :booking, only: [ :show ] do
+     # refacto
     member { patch :accept }
       # 2 eme version non refacto
-    member do 
+    member do
       patch :decline
     end
   end
