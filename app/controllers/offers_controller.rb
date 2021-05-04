0df053ebba3e9290d29f2fb22622b4c7ba89c80d
Teyant Alexandre qextzy@gmail.com <qextzy@gmail.com>
2021-05-04 16:43:17 +0200

    addding cloudinary

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,11 @@
 class OffersController < ApplicationController
 skip_before_action :authenticate_user!, only: :index
+
   def index
     @offers = Offer.all
+  end
+
+  def article_params
+    params.require(:article).permit(:photo)
   end
 end
