9914b293a12ba96d44e42536422f4b68b29a03c5
Teyant Alexandre qextzy@gmail.com <qextzy@gmail.com>
2021-05-05 14:41:32 +0200

    adding new-create and bootstrap

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
 
   def show
     @booking = Booking.find(params[:id])
@@ -14,8 +15,21 @@ class BookingsController < ApplicationController
     # we need `restaurant_id` to associate review with corresponding restaurant
     @offer = Offer.find(params[:offer_id])
     @booking.offer = @offer
-    @booking.save
-    redirect_to offer_path(@offer)
+    @booking.user = current_user
+    @booking.total_price = (@booking.end_date - @booking.start_date + 1).to_i * @offer.price_per_day
+    @booking.status = "Pending"
+    if @booking.save!
+      redirect_to offer_path(@offer)
+    else
+      render :new
+    end
+  end
+
+  def accept
+
+  end
+
+  def decline
   end
 
   private
